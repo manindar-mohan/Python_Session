ab6aef063d864093c8b8aa2e44b81d718069d628
Manieendar Mohan <manieendar.m@appfabs.com>
2021-05-10 13:48:02 +0530

    exception handling added in port scanner

--- port_scanner.py
+++ port_scanner.py
@@ -35,5 +35,5 @@ except socket.gaierror:
         print("\n Hostname Could Not Be Resolved !!!!")
         sys.exit()
 except socket.error:
-        print("\ Server not responding !!!!")
+        print("\nServer not responding !!!!")
         sys.exit()
